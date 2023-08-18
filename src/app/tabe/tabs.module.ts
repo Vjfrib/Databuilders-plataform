f6e0fda6992f137dbefbc2d877b8000ef21e1247
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-08-18 20:50:46 -0300

    teste

---
+++ src/app/tabe/tabs.module.ts
@@ -0,0 +1,19 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { tabePageRoutingModule } from './tabe.router.module';
+
+import { tabePage } from './tabe.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    tabePageRoutingModule
+  ],
+  declarations: [tabePage]
+})
+export class tabePageModule {}
---
+++ src/app/tabe/tabs.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabe',
+  templateUrl: 'tabe.page.html',
+  styleUrls: ['tabe.page.scss']
+})
+export class tabePage {
+
+  constructor() {}
+
+}
