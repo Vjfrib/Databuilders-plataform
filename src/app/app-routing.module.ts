585f25dfd559c9bcf357020bc170622b690add49
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-08-18 21:22:03 -0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'user', loadChildren: './tabe/tabs.module#TabsPageModule' },
+  //{ path: 'user', loadChildren: './tabe/tabe.module#TabsPageModule' },
 ];
 @NgModule({
   imports: [
